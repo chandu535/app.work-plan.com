40efd38dc5589e3ad8c94f96b8d7c43108c5569b
fe-dev2 <fe-dev2@labsquire.com>
2023-11-29 18:02:57 +0530

    updated tasks page redirect url

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,4 +1,4 @@
-const Tasks = () => {
+const TasksComponent = () => {
     return (
         <div>
             <h1>Tasks</h1>
@@ -6,4 +6,4 @@ const Tasks = () => {
     );
 }
 
-export default Tasks;
+export default TasksComponent;
--- src/components/login/LoginComponent.tsx
+++ src/components/login/LoginComponent.tsx
@@ -14,7 +14,7 @@ export default function LoginComponent() {
                 {/* Your content here */}
                 <Typography style={{ color: "black" }}>Welcome to Orotron</Typography>
 
-                <Link style={{ color: "red" }}
+                <Link style={{ color: "blue" }}
                     href={"https://slack.com/openid/connect/authorize?response_type=code&state=4c8a9451-077e-4637-ab7b-8c1849e41f3a&client_id=120000646596.6250899073846&scope=openid,email,profile&redirect_uri=https://app-workplanner.vercel.app/tasks"}
                 >Login with Slack</Link>
             </div>
