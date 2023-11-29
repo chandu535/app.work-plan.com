5882bf494fcb9ac77c8574ec15b36d1c2d68f992
saijoro <sai.j@orotron.com>
2023-11-29 18:31:39 +0530

    Updated Home page dev

--- src/components/login/LoginComponent.tsx
+++ src/components/login/LoginComponent.tsx
@@ -7,18 +7,23 @@ import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 export default function LoginComponent() {
     return (
-        <div className="container">
-            <div className="blur-bg" />
-            <div className="content">
-                <img src={"/orotron_logo.jpeg"} alt="Logo" />
-                {/* Your content here */}
-                <Typography style={{ color: "black" }}>Welcome to Orotron</Typography>
+      <div className="container">
+        <div className="blur-bg" />
+        <div className="content">
+          <img src={"/orotron_logo.jpeg"} alt="Logo" />
+          {/* Your content here */}
+          <Typography style={{ color: "black" }}>Welcome to Orotron</Typography>
 
-                <Link style={{ color: "blue" }}
-                    href={"https://slack.com/openid/connect/authorize?response_type=code&state=4c8a9451-077e-4637-ab7b-8c1849e41f3a&client_id=120000646596.6250899073846&scope=openid,email,profile&redirect_uri=https://app-workplanner.vercel.app/tasks"}
-                >Login with Slack</Link>
-            </div>
+          <Link
+            style={{ color: "green" }}
+            href={
+              "https://slack.com/openid/connect/authorize?response_type=code&state=4c8a9451-077e-4637-ab7b-8c1849e41f3a&client_id=120000646596.6250899073846&scope=openid,email,profile&redirect_uri=https://app-workplanner.vercel.app/tasks"
+            }
+          >
+            Login with Slack
+          </Link>
         </div>
-    )
+      </div>
+    );
 }
 ``
