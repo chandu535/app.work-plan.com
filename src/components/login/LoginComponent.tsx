601cdd7e60f0a5bfe33ff9cc6578b7c7298f9bbc
saijoro <sai.j@orotron.com>
2023-11-30 11:52:57 +0530

    Home Page Code

--- src/components/login/LoginComponent.tsx
+++ src/components/login/LoginComponent.tsx
@@ -15,7 +15,7 @@ export default function LoginComponent() {
           <Typography style={{ color: "black" }}>Welcome to Orotron</Typography>
 
           <Link
-            style={{ color: "yellow" }}
+            style={{ color: "green" }}
             href={
               "https://slack.com/openid/connect/authorize?response_type=code&state=4c8a9451-077e-4637-ab7b-8c1849e41f3a&client_id=120000646596.6250899073846&scope=openid,email,profile&redirect_uri=https://app-work-plan-com.vercel.app/tasks"
             }
